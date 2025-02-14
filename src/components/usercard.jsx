f8a26764261f16c059d6cb29b62128efbefacd0a
shebin <mohammedshebinc92@gmail.com>
2025-02-14 12:00:40 +0530

    react component

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,22 @@
-// write the component code here
+import React from "react";
+import "./usercard.css";
+import profilePhoto from "../assets/image.png"
+
+const Usercard = () => {
+  const name = "Mohammed Shebin";
+  const email = "shebin4444@example.com";
+  const phone = "8909567777";
+  const address = "church Main St, New York, USA";
+
+  return (
+    <div className="user-card">
+        <img src={profilePhoto} alt="Profile" className="profile-photo" />
+        <h2 className="user-name">{name}</h2>
+        <p className="user-email">📧 {email}</p>
+        <p className="user-phone">📞 {phone}</p>
+        <p className="user-address">📍 {address}</p>
+    </div>
+  );
+};
+
+export default Usercard;
